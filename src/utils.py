bb5769b002f2f6e069d9ec7fef212fa2c717dc3e
vinaysaivutukuri <vinaysaivutukuri@gmail.com>
2025-09-08 09:30:09 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -26,12 +27,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
+            
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -51,10 +47,3 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
-
-    except Exception as e:
-        raise CustomException(e, sys)
